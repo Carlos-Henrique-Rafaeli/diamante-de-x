d8690aae55dab32ba273330bf836081c19fc593a
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-25 16:19:15 -0300

    Implementado verificação do Input

--- DiamanteX.ConsoleApp/Program.cs
+++ DiamanteX.ConsoleApp/Program.cs
@@ -4,6 +4,20 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int tamanhoDiamante;
+
+        while (true)
+        {
+            Console.Write("Digite o tamanho do diamante: ");
+
+            bool conseguiuConverter = int.TryParse(Console.ReadLine(), out tamanhoDiamante);
+
+            if (conseguiuConverter && tamanhoDiamante % 2 != 0)
+                break;
+
+            Console.WriteLine("\nValor Inválido\n");
+        }
+
+        Console.ReadLine();
     }
 }
